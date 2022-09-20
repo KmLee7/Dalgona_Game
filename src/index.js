0e6ee6650c58972e723dcdafbb6ca3e67a20c3a2
“Kyungmin <rudalslee7@gmail.com>
2022-09-20 17:49:30 -0400

    fix timer bug

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 
 import { drawStar } from "./scripts/heading";
 
+let start = false;
 
 window.addEventListener('load', () => {
     const canvas = document.querySelector('#canvas');
     const ctx = canvas.getContext('2d');
     
     let painting = false;
-
     function startPosition(e) {
         painting = true;
+            if (painting === true) {
+                if (start === false) {
+                    setInterval(updateCountdown, 1000);
+                    start = true;
+                }
+            }
         draw(e);
-        setInterval(updateCountdown, 1000); // NOT CORRECT
-    
     }
     
     function finishedPosition() {
@@ -24,7 +28,7 @@ window.addEventListener('load', () => {
         if (!painting) return;
         ctx.lineWidth = 5;
         ctx.lineCap = 'round';
-        console.log(e)
+        // console.log(e)
         ctx.lineTo(e.offsetX, e.offsetY);
         ctx.stroke();
         ctx.beginPath();
@@ -42,16 +46,17 @@ const timeLeft = document.getElementById('count-down');
 const startingMinutes = 3;
 let time = startingMinutes * 60;
 
+// setInterval(updateCountdown, 1000); 
+
 function updateCountdown() {
     const minutes = Math.floor(time/60);
     let seconds = time % 60;
     
     seconds = seconds < 10 ? '0' + seconds : seconds;
     
+
     timeLeft.innerHTML = `${minutes}:${seconds}`;
-    if (time === 0) {
-        
-    } else {
+    if (time !== 0) {
         time -= 1;
     }
     
