c24aabb222a8343fd94f0426859849d706edae0e
“Kyungmin <rudalslee7@gmail.com>
2022-09-21 15:33:11 -0400

    fix percentage

--- src/index.js
+++ src/index.js
@@ -3,35 +3,65 @@ import { drawStar } from "./scripts/heading";
 import { drawCircle } from "./scripts/heading";
 import { drawStarCircle } from "./scripts/heading";
 import { newCanvas } from "./scripts/heading";
+import { starpoints } from "./scripts/points";
+import { circlepoints } from "./scripts/points";
 
 const clearCanvas = document.getElementById('newGame');
 const levelOne = document.getElementById('one');
 const levelTwo = document.getElementById('two');
 const levelThree = document.getElementById('three');
 const resultDisplay = document.getElementById('resultId');
-import { starpoints } from "./scripts/points";
+const perc = document.getElementById('percentage');
+var timer;
+// let start = false;
 
-let start = false;
-// const timerId = setInterval(updateCountdown,1000);
+const timeLeft = document.getElementById('count-down');
+const startingMinutes = 1; // change back to 3 after debugging.
+let time = startingMinutes * 10; // change 3 to 60 after debugging. 
 
 window.addEventListener('load', () => {
     let canvas = document.querySelector('#canvas');
     let ctx = canvas.getContext('2d');
     let painting = false;
     
-    class Player {
-        constructor() {
-            this.points = new Set();
-        }
+class Player {
+    constructor() {
+        this.points = new Set();
+        this.ongoing = false;
+        this.level = 0;
     }
+    timeStart() {
+        this.ongoing = true;
+        timer = setInterval(() => {
+            const minutes = Math.floor(time/60);
+            let seconds = time % 60;
+            seconds = seconds < 10 ? '0' + seconds : seconds;
+            console.log('going')
+            timeLeft.innerHTML = `${minutes}:${seconds}`;
+            if (time > 0) {
+                time -= 1;
+            } else {
+                this.timeEnd();
+            }
+        },1000);
+    }
+    timeEnd() {
+        console.log('stopped');
+        this.ongoing = false;
+        clearInterval(timer);
+    }
+    restartTime() {
+        time = startingMinutes * 3;
+        perc.textContent = 0;
+    }
+}
     let player = new Player();
+
     function startPosition(e) {
         painting = true;
-        if (painting === true) {
-            if (start === false) {
-                var timer = setInterval(updateCountdown, 1000);
-                timer;
-                start = true;
+        if (painting) {
+            if (player.ongoing === false) {
+                player.timeStart();
             }
         }
         draw(e);
@@ -48,69 +78,79 @@ window.addEventListener('load', () => {
         ctx.lineCap = 'round';
         ctx.strokeStyle = 'white'
         ctx.lineTo(e.offsetX, e.offsetY);
-        starpoints.forEach(point =>  {
-           if (Math.abs(e.offsetX - point[0]) <= 8 && Math.abs(e.offsetY - point[1]) <= 8) {
+        if (player.level === 1) {
+            circlepoints.forEach(point => {
+            if (Math.abs(e.offsetX - point[0]) <= 8 && Math.abs(e.offsetY - point[1]) <= 8) {
                 player.points.add(point);
+            }
+        });
+    }
+        if (player.level === 2) {
+            starpoints.forEach(point =>  {
+            if (Math.abs(e.offsetX - point[0]) <= 8 && Math.abs(e.offsetY - point[1]) <= 8) {
+                    player.points.add(point);
            }
         });
-        // console.log(player.points, 'the pooijnts');
-        // console.log(e.offsetX, e.offsetY);
+    }
         ctx.stroke();
         ctx.beginPath();
         ctx.moveTo(e.offsetX,e.offsetY);
-        gameDone();
-        // lose();
-        win();
+        if (timer === 0) {
+            gameDone();
+            // lose();
+            
+        }
+        if (player.points.size >= (starpoints.length * .80)) {
+            win();
+        }
+         perc.textContent = ` ${Math.round((player.points.size / starpoints.length) * 100)}%`;
     }
     
     canvas.addEventListener('mousedown', startPosition);
     canvas.addEventListener('mouseup', finishedPosition);
     canvas.addEventListener('mousemove', draw); //e => draw(e));
-    clearCanvas.addEventListener('click', () => {
+    clearCanvas.addEventListener('click', () => { // New Game
         ctx.reset();
         newCanvas();
+        player.restartTime();
+        player.points = new Set()
     });
-    levelOne.addEventListener('click', () => {
+    levelOne.addEventListener('click', () => { // Level 1
         ctx.reset();
         newCanvas();
         drawCircle();
+        player.restartTime();
+        player.level = 1;
+        player.points = new Set()
     });
-    levelTwo.addEventListener('click', () => {
+    levelTwo.addEventListener('click', () => { // Level 2
         ctx.reset();
         newCanvas();
         drawStar();
+        player.restartTime();
+        player.level = 2;
+        player.points = new Set()
     });
-    levelThree.addEventListener('click', () => {
+    levelThree.addEventListener('click', () => { // Level 3
         ctx.reset();
         newCanvas();
         drawStarCircle();
+        player.restartTime();
+        player.level = 3;
+        player.points = new Set()
     });
 
-const timeLeft = document.getElementById('count-down');
-const startingMinutes = 1; // change back to 3 after debugging.
-let time = startingMinutes * 10; // change 3 to 60 after debugging. 
 
-function updateCountdown() {
-    const minutes = Math.floor(time/60);
-    let seconds = time % 60;
-    if (start === false) {
-        seconds = seconds < 10 ? '0' + seconds : seconds;
-        
-        timeLeft.innerHTML = `${minutes}:${seconds}`;
-        
-        if (time !== 0) {
-            time -= 1;
-        }
-    }
-}
+
+
 
     function win() {
-        if (time !== 0) {
-            if (player.points.size >= (starpoints.length * .80)) {
-                resultDisplay.textContent = 'You win!';
-                clearInterval(timer);
-            }
-        }
+        console.log(player.points.size,'points');
+        console.log(starpoints.length, 'total');
+        console.log(circlepoints.length, 'total')
+        resultDisplay.textContent = 'You win!';
+
+        
     }
 
     // function lose() {
